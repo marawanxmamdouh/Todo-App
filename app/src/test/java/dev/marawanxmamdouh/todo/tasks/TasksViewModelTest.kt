43123805228e5fa43eafb76af31b3748578d121f
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-09-08 23:04:29 +0200

    Test Live Data

--- app/src/test/java/dev/marawanxmamdouh/todo/tasks/TasksViewModelTest.kt
+++ app/src/test/java/dev/marawanxmamdouh/todo/tasks/TasksViewModelTest.kt
@@ -1,12 +1,21 @@
 package dev.marawanxmamdouh.todo.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.marawanxmamdouh.todo.getOrAwaitValue
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.nullValue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -18,5 +27,7 @@ class TasksViewModelTest {
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
     }
 }
